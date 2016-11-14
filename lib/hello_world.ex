fadd3c5498864731fef14586843994b65fe41334
Alex Sghia-Hughes <alex@mbtace.com>
2016-11-14 10:27:48 -0500

    50% coverage commit

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -1,2 +1,9 @@
 defmodule HelloWorld do
+  def add(x, y) do
+    x + y
+  end
+
+  def subtract(x, y) do
+    x - y
+  end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule HelloWorld.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
      deps: deps()]
   end
 
@@ -27,6 +28,8 @@ defmodule HelloWorld.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.5", only: :test}
+    ]
   end
 end
